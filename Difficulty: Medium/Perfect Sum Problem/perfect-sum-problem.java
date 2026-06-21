82b83fc6ac27f98499cccfebb9ff247bbb6ca12d
MohdTalib-twenty <71248611+MohdTalib-twenty@users.noreply.github.com>
2026-06-21 23:03:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
@@ -14,20 +14,31 @@ class Solution {
         return  dp[i][target]= t + nt;
     }
     // Function to calculate the number of subsets with a given sum
-    public int perfectSum(int[] nums, int target) {
+    public int perfectSum(int[] arr, int sum) {
         // code here
-        int n = nums.length;
-        int c=0;
-        // if(target == 0){
-        //     for(int i=0;i<n;i++){
-        //         if(nums[i] == 0)c++;
-        //     }
-        //     return (int)Math.pow(2,c);
-        // }
-        int [][] dp = new int[n][target+1] ;
+        int n = arr.length;
+        //int sum = 0;
+        // for(int i=0;i<n;i++)sum+=arr[i];
+        int[][] dp = new int[n+1][sum+1];
+        
         for(int[] rows: dp){
-            Arrays.fill(rows,-1);
+            Arrays.fill(rows, -1);
         }
-        return Solve(0,n,nums,target,dp);
+        // Solve(0,n,arr,sum,dp);
+        dp[n][0]=1;
+        for(int j=1;j<=sum;j++){
+            dp[n][j]=0;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=sum;j>=0;j--){
+                int nt = dp[i+1][j];
+                int t = 0;
+                if(arr[i]<=j){
+                    t = dp[i+1][j-arr[i]];
+                }
+                dp[i][j] = t+nt;
+            }
+        }
+        return dp[0][sum];
     }
 }
